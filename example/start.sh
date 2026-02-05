c029d644c793ffdefa084427627d42dfc315e736
qpzmV <849840504@qq.com>
2026-02-05 17:45:25 +0800

    update start.sh

--- example/start.sh
+++ example/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nitriding -fqdn example.com -ext-pub-port 443 -intport 8080 -wait-for-app &
+nitriding -fqdn example.com -ext-pub-port 10443 -intport 8080 -wait-for-app &
 echo "[sh] Started nitriding."
 
 sleep 1
